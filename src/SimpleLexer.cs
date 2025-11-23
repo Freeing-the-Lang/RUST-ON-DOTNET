9bcb88b34e7da20a5f71ad19ec77b783b526b2fe
한진희 <0200134hjh@gmail.com>
2025-11-23 10:52:30 +0900

    Create SimpleLexer.cs

---
+++ src/SimpleLexer.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Collections.Generic;
+
+namespace RustOnDotnet
+{
+    public enum TokenKind { Ident, Number, Symbol, Keyword, EOF }
+
+    public record Token(TokenKind Kind, string Text);
+
+    public class SimpleLexer
+    {
+        private readonly string _src;
+        private int _i = 0;
+
+        private static readonly HashSet<string> Keywords = new()
+        {
+            "fn", "let", "mut", "return"
+        };
+
+        public SimpleLexer(string src) => _src = src;
+
+        public Token Next()
+        {
+            Skip();
+
+            if (_i >= _src.Length)
+                return new Token(TokenKind.EOF, "");
+
+            char c = _src[_i];
+
+            if (char.IsLetter(c))
+                return Ident();
+
+            if (char.IsDigit(c))
+                return Number();
+
+            return new Token(TokenKind.Symbol, _src[_i++].ToString());
+        }
+
+        private Token Ident()
+        {
+            int s = _i;
+            while (_i < _src.Length && char.IsLetterOrDigit(_src[_i]))
+                _i++;
+
+            string word = _src[s.._i];
+            if (Keywords.Contains(word))
+                return new Token(TokenKind.Keyword, word);
+
+            return new Token(TokenKind.Ident, word);
+        }
+
+        private Token Number()
+        {
+            int s = _i;
+            while (_i < _src.Length && char.IsDigit(_src[_i]))
+                _i++;
+
+            return new Token(TokenKind.Number, _src[s.._i]);
+        }
+
+        private void Skip()
+        {
+            while (_i < _src.Length && char.IsWhiteSpace(_src[_i]))
+                _i++;
+        }
+    }
+}
