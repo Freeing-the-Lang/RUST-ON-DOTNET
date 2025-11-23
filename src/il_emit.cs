52e86b07014edeb94c36f0c9bcdb94d833a85d7b
한진희 <0200134hjh@gmail.com>
2025-11-23 10:59:26 +0900

    Update il_emit.cs

--- src/il_emit.cs
+++ src/il_emit.cs
@@ -1,13 +1,10 @@
-// File: il_emit.cs
-// IL emitter skeleton (not generating real IL yet)
-
 using System;
 using System.Reflection;
 using System.Reflection.Emit;
 
 namespace RustOnDotnet
 {
-    public class ILGeneratorPrototype
+    public class ILEmit
     {
         public void EmitHello()
         {
@@ -15,22 +12,21 @@ namespace RustOnDotnet
                 new AssemblyName("RustDotnetGen"),
                 AssemblyBuilderAccess.RunAndCollect);
 
-            var module = asm.DefineDynamicModule("MainMod");
-            var type = module.DefineType("RustMain");
+            var mod = asm.DefineDynamicModule("MainMod");
+            var type = mod.DefineType("RustMain");
 
             var method = type.DefineMethod(
                 "main",
                 MethodAttributes.Public | MethodAttributes.Static);
 
             var il = method.GetILGenerator();
-            il.Emit(OpCodes.Ldstr, "Rust-on-Dotnet IL running!");
-            il.Emit(OpCodes.Call, typeof(Console).GetMethod("WriteLine", new[] { typeof(string) }));
+            il.Emit(OpCodes.Ldstr, "Rust-on-Dotnet IL: OK");
+            il.Emit(OpCodes.Call, typeof(Console)
+                .GetMethod("WriteLine", new[] { typeof(string) }));
             il.Emit(OpCodes.Ret);
 
-            var finalType = type.CreateType();
-
-            // Invoke
-            finalType.GetMethod("main")?.Invoke(null, null);
+            var t = type.CreateType();
+            t.GetMethod("main")?.Invoke(null, null);
         }
     }
 }
