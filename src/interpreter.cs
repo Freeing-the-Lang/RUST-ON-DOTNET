6bbdafae583614828ae054fcd6d2d38b6aec17d7
한진희 <0200134hjh@gmail.com>
2025-11-23 10:59:04 +0900

    Update interpreter.cs

--- src/interpreter.cs
+++ src/interpreter.cs
@@ -1,5 +1,3 @@
-// File: interpreter.cs
-
 using System;
 using System.Collections.Generic;
 
@@ -7,29 +5,69 @@ namespace RustOnDotnet
 {
     public class Interpreter
     {
-        private readonly Dictionary<string, int> Env = new();
+        private readonly Stack<Dictionary<string,int>> Scopes = new();
+
+        public Interpreter()
+        {
+            Scopes.Push(new Dictionary<string,int>());
+        }
+
+        private void Set(string name, int val)
+            => Scopes.Peek()[name] = val;
+
+        private int Get(string name)
+        {
+            foreach (var sc in Scopes)
+                if (sc.ContainsKey(name))
+                    return sc[name];
+            throw new Exception($"Unknown variable {name}");
+        }
 
         public int Eval(Node n)
         {
             return n switch
             {
-                NumberLit num => num.Value,
+                NumberLit num      => num.Value,
+                IdentExpr id       => Get(id.Name),
 
-                IdentExpr id =>
-                    Env.ContainsKey(id.Name)
-                        ? Env[id.Name]
-                        : throw new Exception($"Unknown variable: {id.Name}"),
+                LetDecl let        => (Set(let.Name, Eval(let.Value)), 0).Item2,
 
-                LetDecl let =>
-                (
-                    Env[let.Name] = Eval(let.Value)
-                ),
+                Binary b           => EvalBin(b),
 
-                Binary bin when bin.Op == "+" =>
-                    Eval(bin.Left) + Eval(bin.Right),
+                ReturnStmt r       => Eval(r.Expr),
 
-                _ => throw new Exception("Unsupported node")
+                Block blk          => EvalBlock(blk),
+
+                _ => throw new Exception("Unsupported node: " + n)
+            };
+        }
+
+        private int EvalBlock(Block b)
+        {
+            Scopes.Push(new Dictionary<string,int>());
+
+            int last = 0;
+            foreach (var s in b.Stmts)
+                last = Eval(s);
+
+            Scopes.Pop();
+            return last;
+        }
+
+        private int EvalBin(Binary b)
+        {
+            int l = Eval(b.L);
+            int r = Eval(b.R);
+
+            return b.Op switch
+            {
+                "+" => l + r,
+                "-" => l - r,
+                "*" => l * r,
+                "/" => r == 0 ? throw new Exception("div0") : l / r,
+                _ => throw new Exception("Unknown op: " + b.Op)
             };
         }
     }
 }
+
